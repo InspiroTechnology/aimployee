2ab214b76651f668c28f4e2083668c62373fc578
Simon Shen <shentiexiong@gmail.com>
2025-03-19 22:14:57 +1300

    Enhance file upload page with improved UI and multiple drop zones

--- src/app/features/upload/upload-page/upload-page.component.ts
+++ src/app/features/upload/upload-page/upload-page.component.ts
@@ -10,16 +10,41 @@ import { CommonModule } from '@angular/common';
   styleUrl: './upload-page.component.scss',
 })
 export class UploadPageComponent {
-  uploader: FileUploader;
-  hasBaseDropZoneOver: boolean = false;
+  uploader:FileUploader;
+  hasBaseDropZoneOver:boolean;
+  hasAnotherDropZoneOver:boolean;
+  response:string;
 
-  constructor() {
+  constructor (){
     this.uploader = new FileUploader({
-      url: 'https://your-api-endpoint.com/upload',
-      authToken: localStorage.getItem('access_token') || '',
-      isHTML5: true
+      url: 'https://evening-anchorage-3159.herokuapp.com/api/',
+      disableMultipart: true, // 'DisableMultipart' must be 'true' for formatDataFunction to be called.
+      formatDataFunctionIsAsync: true,
+      formatDataFunction: async (item:any) => {
+        return new Promise( (resolve, reject) => {
+          resolve({
+            name: item._file.name,
+            length: item._file.size,
+            contentType: item._file.type,
+            date: new Date()
+          });
+        });
+      }
     });
+
+    this.hasBaseDropZoneOver = false;
+    this.hasAnotherDropZoneOver = false;
+
+    this.response = '';
+
+    this.uploader.response.subscribe( res => this.response = res );
   }
 
+  public fileOverBase(e:any):void {
+    this.hasBaseDropZoneOver = e;
+  }
 
+  public fileOverAnother(e:any):void {
+    this.hasAnotherDropZoneOver = e;
+  }
 }
