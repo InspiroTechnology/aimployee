6ed730d7f9fd66daa185b3f98e0254afce28602b
Simon Shen <shentiexiong@gmail.com>
2025-03-19 22:10:03 +1300

    Implement file upload functionality with ng2-file-upload integration

--- src/app/features/upload/upload-page/upload-page.component.ts
+++ src/app/features/upload/upload-page/upload-page.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FileUploadModule, FileUploader } from 'ng2-file-upload';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-upload-page',
   standalone: true,
-  imports: [],
+  imports: [FileUploadModule,CommonModule], // Add Ng2FileUploadModule here
   templateUrl: './upload-page.component.html',
-  styleUrl: './upload-page.component.scss'
+  styleUrl: './upload-page.component.scss',
 })
 export class UploadPageComponent {
+  uploader: FileUploader;
+  hasBaseDropZoneOver: boolean = false;
+
+  constructor() {
+    this.uploader = new FileUploader({
+      url: 'https://your-api-endpoint.com/upload',
+      authToken: localStorage.getItem('access_token') || '',
+      isHTML5: true
+    });
+  }
+
 
 }
