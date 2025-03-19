aee8154c0c60aacdee5b16390136895a5acf76d2
Simon Shen <shentiexiong@gmail.com>
2025-03-19 22:22:31 +1300

    Refactor upload page component: remove unused styles, enable view encapsulation, and improve code formatting

--- src/app/features/upload/upload-page/upload-page.component.ts
+++ src/app/features/upload/upload-page/upload-page.component.ts
@@ -1,35 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { FileUploadModule, FileUploader } from 'ng2-file-upload';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-upload-page',
   standalone: true,
-  imports: [FileUploadModule,CommonModule], // Add Ng2FileUploadModule here
+  imports: [FileUploadModule, CommonModule],
   templateUrl: './upload-page.component.html',
   styleUrl: './upload-page.component.scss',
+  encapsulation: ViewEncapsulation.None, // Disable encapsulation
 })
 export class UploadPageComponent {
-  uploader:FileUploader;
-  hasBaseDropZoneOver:boolean;
-  hasAnotherDropZoneOver:boolean;
-  response:string;
+  uploader: FileUploader;
+  hasBaseDropZoneOver: boolean;
+  hasAnotherDropZoneOver: boolean;
+  response: string;
 
-  constructor (){
+  constructor() {
     this.uploader = new FileUploader({
       url: 'https://evening-anchorage-3159.herokuapp.com/api/',
       disableMultipart: true, // 'DisableMultipart' must be 'true' for formatDataFunction to be called.
       formatDataFunctionIsAsync: true,
-      formatDataFunction: async (item:any) => {
-        return new Promise( (resolve, reject) => {
+      formatDataFunction: async (item: any) => {
+        return new Promise((resolve, reject) => {
           resolve({
             name: item._file.name,
             length: item._file.size,
             contentType: item._file.type,
-            date: new Date()
+            date: new Date(),
           });
         });
-      }
+      },
     });
 
     this.hasBaseDropZoneOver = false;
@@ -37,14 +38,14 @@ export class UploadPageComponent {
 
     this.response = '';
 
-    this.uploader.response.subscribe( res => this.response = res );
+    this.uploader.response.subscribe((res) => (this.response = res));
   }
 
-  public fileOverBase(e:any):void {
+  public fileOverBase(e: any): void {
     this.hasBaseDropZoneOver = e;
   }
 
-  public fileOverAnother(e:any):void {
+  public fileOverAnother(e: any): void {
     this.hasAnotherDropZoneOver = e;
   }
 }
