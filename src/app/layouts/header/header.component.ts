7902b38cfc4d0b9a14379ad862eb2b55426e4d1b
Simon Shen <shentiexiong@gmail.com>
2025-03-20 16:06:34 +1300

    Add sidebar toggle button to header: implement click functionality to control sidebar visibility

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { LayOutService } from '../../service/lay-out.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, RouterModule],
+  imports: [MatToolbarModule, MatButtonModule, RouterModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor(public layoutService: LayOutService) {}
+
   // No additional logic needed for a static Angular Material nav bar
 }
