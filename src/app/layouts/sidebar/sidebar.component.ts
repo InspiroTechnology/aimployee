8cdb3c97de0366108df97aad429f523334cd928a
Simon Shen <shentiexiong@gmail.com>
2025-03-20 16:17:22 +1300

    Enhance sidebar toggle button: update button to use mat-icon-button for consistency and improve layout structure

--- src/app/layouts/sidebar/sidebar.component.ts
+++ src/app/layouts/sidebar/sidebar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LayOutService } from '../../service/lay-out.service';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule,MatButtonModule],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'], // Fixed typo here
 })
