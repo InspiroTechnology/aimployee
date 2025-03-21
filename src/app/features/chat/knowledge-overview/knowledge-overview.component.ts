1a9099ce95c18b9edce2fb80d76b5a951c335912
Simon Shen <shentiexiong@gmail.com>
2025-03-21 21:58:33 +1300

    Refactor KnowledgeOverviewComponent and SimpleCardComponent for improved code clarity and structure

--- src/app/features/chat/knowledge-overview/knowledge-overview.component.ts
+++ src/app/features/chat/knowledge-overview/knowledge-overview.component.ts
@@ -7,21 +7,19 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-knowledge-overview',
   standalone: true,
-  imports: [CommonModule,SimpleCardComponent,MatCardModule,MatIconModule],
+  imports: [CommonModule, SimpleCardComponent, MatCardModule, MatIconModule],
   templateUrl: './knowledge-overview.component.html',
-  styleUrls: ['./knowledge-overview.component.scss']
+  styleUrls: ['./knowledge-overview.component.scss'],
 })
 export class KnowledgeOverviewComponent implements OnInit {
   overviewItems = [
-    { label: 'Knowledge Bases', icon: 'park' ,value:0},
-    { label: 'Sources', icon: 'description' ,value:0},
-    { label: 'Seeds', icon: 'spa' ,value:0},
-    { label: 'Tokens', icon: 'widgets',value:0 }
+    { label: 'Bases', icon: 'park', value: 0 },
+    { label: 'Sources', icon: 'description', value: 0 },
+    { label: 'Seeds', icon: 'spa', value: 0 },
+    { label: 'Tokens', icon: 'widgets', value: 0 },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
