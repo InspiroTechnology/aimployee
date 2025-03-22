e763988d54e720eba65222e4db7028f17de3545a
Simon Shen <shentiexiong@gmail.com>
2025-03-22 15:45:57 +1300

    Add AuthLayoutComponent with basic structure and styles

---
+++ src/app/auth/components/auth-layout/auth-layout.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-auth-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './auth-layout.component.html',
+  styleUrls: ['./auth-layout.component.scss']
+})
+export class AuthLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
